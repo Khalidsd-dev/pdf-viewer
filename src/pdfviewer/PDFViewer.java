e3779acd4cbcd904cfc77c209cc77f27544e520d
unknown <shadrackkhalidduma02@gmail.com>
2025-07-23 19:32:55 +0200

    Text-Viewer

---
+++ src/pdfviewer/PDFViewer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdfviewer;
+
+import pdfviewer.viewer.frmMain;
+
+/**
+ *
+ * @author Shadrack
+ */
+public class PDFViewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMain main = new frmMain();
+        main.show();
+    }
+    
+}
